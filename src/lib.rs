2b292530782da9d9710c21da1ef5e2ad3119a168
Sebastian <Sebastian@Strobl.net>
2020-06-11 10:21:21 +0200

    Update: Refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,110 +1,11 @@
-use std::time::{Duration, Instant, SystemTime};
+mod stopwatch;
 
-enum StopwatchState {
-    Running,
-    Paused,
-}
-
-pub struct Stopwatch {
-    state: StopwatchState,
-    duration: Duration,
-    instant: Instant,
-}
-
-impl Stopwatch {
-    pub fn new() -> Self {
-        Self::default()
-    }
-
-    pub fn elapsed(&self) -> Duration {
-        match self.state {
-            StopwatchState::Running => {
-                self.duration + self.instant.elapsed()
-            },
-            StopwatchState::Paused => {
-                self.duration
-            },
-        }
-
-    }
-
-    pub fn start(&mut self) {
-        self.duration = Duration::new(0, 0);
-        self.instant = Instant::now();
-        self.state = StopwatchState::Running;
-    }
-
-    pub fn pause(&mut self) {
-        self.duration += self.instant.elapsed();
-        self.state = StopwatchState::Paused;
-    }
-
-    pub fn unpause(&mut self) {
-        self.instant = Instant::now();
-        self.state = StopwatchState::Running;
-    }
-}
-
-impl Default for Stopwatch {
-    fn default() -> Self {
-        Self {
-            state: StopwatchState::Running,
-            duration: Duration::new(0, 0),
-            instant: Instant::now(),
-        }
-    }
-}
+// public exports
+pub use stopwatch::*;
 
 
-pub struct SystemStopwatch {
-    state: StopwatchState,
-    duration: Duration,
-    instant: SystemTime,
-}
+// TESTS
 
-impl SystemStopwatch {
-    pub fn new() -> Self {
-        Self::default()
-    }
-
-    pub fn elapsed(&self) -> Duration {
-        match self.state {
-            StopwatchState::Running => {
-                self.duration + self.instant.elapsed().unwrap()
-            },
-            StopwatchState::Paused => {
-                self.duration
-            },
-        }
-
-    }
-
-    pub fn start(&mut self) {
-        self.duration = Duration::new(0, 0);
-        self.instant = SystemTime::now();
-        self.state = StopwatchState::Running;
-    }
-
-    pub fn pause(&mut self) {
-        self.duration += self.instant.elapsed().unwrap();
-        self.state = StopwatchState::Paused;
-    }
-
-    pub fn unpause(&mut self) {
-        self.instant = SystemTime::now();
-        self.state = StopwatchState::Running;
-    }
-}
-
-impl Default for SystemStopwatch {
-    fn default() -> Self {
-        Self {
-            state: StopwatchState::Running,
-            duration: Duration::new(0, 0),
-            instant: SystemTime::now(),
-        }
-    }
-}
 
 #[cfg(test)]
 mod tests {
